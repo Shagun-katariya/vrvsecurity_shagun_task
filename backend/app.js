756e780a9d4f55b8fcbfea54efa129e4e8b83037
Shagun Katariya <katariyashagun225@gmail.com>
2024-11-22 21:21:00 +0530

    final

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
-const roleRoutes = require('./routes/roleRoutes');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+const roleRoutes = require("./routes/roleRoutes");
 
 const app = express();
-const cors = require('cors');
-app.use(cors());
-
+const cors = require("cors");
+app.use(cors({ origin: "https://vrvsecurity-shagun-frontend.vercel.app" }));
 
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URL)
-    .then((data) => {
-      console.log(`mongodb connected with server: ${data.connection.host}`);
-    });
+  mongoose.connect(process.env.DB_URL).then((data) => {
+    console.log(`mongodb connected with server: ${data.connection.host}`);
+  });
 };
 
 connectDatabase();
@@ -25,9 +22,9 @@ connectDatabase();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/roles', roleRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/roles", roleRoutes);
 
 app.listen(5000, () => {
-  console.log('Server is running on http://localhost:5000');
+  console.log("Server is running on http://localhost:5000");
 });
